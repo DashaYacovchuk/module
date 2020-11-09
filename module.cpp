3374d1950173e4323ba2bed422483da669f8ab10
DashaYacovchuk <72378051+DashaYacovchuk@users.noreply.github.com>
2020-11-09 13:16:33 +0200

    Add files via upload

--- module.cpp
+++ module.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	int a;
+	vector<int> numbers;
 	while (true) {
 		cin >> a;
-
-
+		numbers.push_back(a);
+		if (a > 100) { break; }
 	}
+	int min = *min_element(numbers.begin(), numbers.end());
+	for (int x : numbers)
+		if (x != min)
+			cout << x << " ";
+	cout << endl;
 	return 0;
 }
